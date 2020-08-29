9eaa6fb4461b25e1fad7f72b2f96fabc0be212c4
Cris-Rider <cristobalgl46@gmail.cl>
2020-08-28 22:56:17 -0400

    Tipos de datos en JS

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-/*Para correr el archivo js se debe escribir en la terminal "node (nombre del archivo).js */
-console.log('Hola, este es mi primer console.log') /* imprime lo que se encuentre dentro de ('') */
+//Para correr el archivo js se debe escribir en la terminal "node (nombre del archivo).js
+console.log('Hola Mundo, este es mi primer console.log') // imprime lo que se encuentre dentro de ('') 
+
+//TIPOS DE DATOS EN JS
+//string: cadena de "caracteres".
+//boolean: true/false.
+//null: nulo.
+//number: como un int.
+//undefinied.
+//object: objeto.

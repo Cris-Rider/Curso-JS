9c7da240aa9422a0acb6e27f1ad02035cc88d429
Cris-Rider <cristobalgl46@gmail.cl>
2020-09-01 19:17:37 -0400

    Operadores logicos en js

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ arrayvacio.push(Alumno.fecha_de_nacimiento)
 
 //console.log(array, arrayvacio);//desplegamos ambos arrays
 
-//Operadores
+//OPERADORES MATEMATICOS
 const suma = 1+2
 const resta= 1-3
 const multiplicar = 2*4
@@ -56,15 +56,31 @@ const num= 5
 
 let numero= 6//let permite incrementar la variable
 numero++
-console.log(numero);
+//console.log(numero);
 
 numero--//decrementa la variable en -1
 numero--
-console.log(numero);
+//console.log(numero);
 
 let num2= 5
 num2+= 5 //incrementa la variable en lo que se declara
 num2-= 5//se resta 5
 num2*= 5//luego se multiplica por 5
 num2/= 2//y finalmente se divide en 2
-console.log(num2);
+//console.log(num2);
+
+//OPERADORES LOGICOS
+//retornan valores booleanos true/false
+const resultado1 = 5 === 6 //compara los valores de las variables y el tipo de variables
+
+const resultado2 = 3 == '3' //compara los valores de las varibles sin importar el tipo de variable
+
+const resultado3 = 4 < 5 //true
+const resultado4 = 4 > 4 //false
+const resultado5 = 3 <= 4//true
+const resultado6 = 4 >= 4//true
+
+const resultado7 = 5!==6 //ve si son estrictamente desiguales o no
+const resultado8 = 5!= '5'
+
+console.log(resultado1, resultado2, resultado3, resultado4, resultado5, resultado6, resultado7, resultado8);

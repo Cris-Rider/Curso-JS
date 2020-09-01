e69cfade2ec436bd13da7a4cbe21b81ac836e2d1
Cris-Rider <cristobalgl46@gmail.cl>
2020-09-01 18:12:16 -0400

    Variables boolean, undefinied, null y object

--- index.js
+++ index.js
@@ -18,6 +18,41 @@ console.log('Hola Mundo, este es mi primer console.log') // imprime lo que se en
 
 let miPrimeraVariable = 'primera variable'
 console.log(miPrimeraVariable);
-
+// mutabilidad
 miPrimeraVariable = 'he reasignado el valor'
 console.log(miPrimeraVariable);
+
+//boolean
+let miBoolean = true
+let miotroBoolean = false
+
+let minumero = 0
+let minumero2 = 12
+let minumero3 = -123
+
+console.log(minumero, minumero2, miBoolean, miPrimeraVariable);
+
+let undef //varibale indefinida
+console.log(undef);
+
+let nulo = null //variable null
+console.log(nulo);
+
+//Objeto vacio
+const miPrimerObjeto = {}
+
+//Objeto
+const miObjeto = {
+    numero: 12,
+    string: 'cadena de caracteres',
+    condicion: true,
+}
+
+console.log(miObjeto);
+
+const Alumno ={
+    fecha_de_nacimiento:'26/12/1997',
+    Año_escolar:10 //contando de 1 a 12, 1 siendo primero basico, 12 siendo cuarto medio.
+}
+
+console.log('La edad del alumno pedro es:',Alumno.fecha_de_nacimiento);
